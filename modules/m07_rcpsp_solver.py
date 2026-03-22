59c7a5f18cba6e4feb5629a168d43d922b73e52c
SPSS <118935216+paavan-showri@users.noreply.github.com>
2026-03-22 01:17:42 -0500

    Update m07_rcpsp_solver.py

--- modules/m07_rcpsp_solver.py
+++ modules/m07_rcpsp_solver.py
@@ -51,7 +51,7 @@ def solve_rcpsp(tasks: List[Task], capacities: Dict[str, int]) -> pd.DataFrame:
         rows.append({
             "Task ID": task.task_id,
             "Task Name": task.name,
-            "Duration (sec)": task.duration_sec,
+            "Duration": task.duration_sec,
             "Start (sec)": solver.Value(starts[task.task_id]),
             "End (sec)": solver.Value(ends[task.task_id]),
             "Immediate Predecessors": task.predecessors,
